42eccd2ac2da955fa6a54aca914604a6fa105ae9
Rob Ray <robert.ray@jpl.nasa.gov>
2020-11-21 11:31:44 -0800

    Rough out UI panels. Write scripts to sort by model name and date.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menu;
+
+    // public Button test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // test.onClick.AddListener(AtoZButtonClicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ExitButtonClicked() 
+    {
+        menu.SetActive(false);
+    }
+
+    public void AtoZButtonClicked()
+    {
+        ModelPanel[] panelArray = FindObjectsOfType<ModelPanel>();
+        List<string> modelPanelList = new List<string>();
+        foreach (ModelPanel item in panelArray)
+        {
+            modelPanelList.Add(item.name);
+        }
+
+        modelPanelList.Sort();
+        int position = 0;
+        foreach (string item in modelPanelList)
+        {
+            print(item);
+            foreach (ModelPanel panelItem in panelArray)
+            {
+                if (panelItem.name == item)
+                {
+                    print(panelItem.name + " " + item + " " + position);
+                    panelItem.transform.SetSiblingIndex(position);
+                }
+            }
+            position++;
+        }
+
+    }
+
+    public void ZtoAButtonClicked()
+    {
+        ModelPanel[] panelArray = FindObjectsOfType<ModelPanel>();
+        List<string> modelPanelList = new List<string>();
+        foreach (ModelPanel item in panelArray)
+        {
+            modelPanelList.Add(item.name);
+        }
+        modelPanelList.Sort();
+        int position = modelPanelList.Count-1;
+        foreach (string item in modelPanelList)
+        {
+            print(item);
+            foreach (ModelPanel panelItem in panelArray)
+            {
+                if (panelItem.name == item)
+                {
+                    print(panelItem.name + " " + item + " " + position);
+                    panelItem.transform.SetSiblingIndex(position);
+                }
+            }
+            position--;
+        }
+
+    }
+
+    public void NewOld()
+    {
+        ModelPanel[] panelArray = FindObjectsOfType<ModelPanel>();
+        List<string> modelPanelList = new List<string>();
+        foreach (ModelPanel item in panelArray)
+        {
+            modelPanelList.Add(item.name);
+        }
+        modelPanelList.Sort();
+        int position = modelPanelList.Count - 1;
+        foreach (string item in modelPanelList)
+        {
+            print(item);
+            foreach (ModelPanel panelItem in panelArray)
+            {
+                if (panelItem.name == item)
+                {
+                    print(panelItem.name + " " + item + " " + position);
+                    panelItem.transform.SetSiblingIndex(position);
+                }
+            }
+            position--;
+        }
+    }
+
+    public void OldNew()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/ModelPanel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModelPanel : MonoBehaviour
+{
+    [SerializeField]
+    public string name, description, creator, date;
+
+    [SerializeField]
+    Image icon;
+
+    [SerializeField]
+    TextMeshProUGUI nameText, descriptionText, creatorText, dateText;
+
+    [SerializeField]
+    DateTime lastUsedDate;
+
+    [SerializeField]
+    float daysFromToday;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastUsedDate = DateTime.Today.AddDays(daysFromToday);
+        nameText.text = name;
+        descriptionText.text = description;
+        creatorText.text = creator;
+        dateText.text = System.DateTime.Now.ToString();
+        if (lastUsedDate.Date == DateTime.Today)
+        {
+            dateText.text = System.DateTime.Now.ToString("HH:mm") + " "  + DateTime.Now.ToString("tt", CultureInfo.InvariantCulture);
+        } 
+        else if (lastUsedDate.Date == DateTime.Today.AddDays(-1))
+        {
+            dateText.text = "Yesterday";
+        } 
+        else 
+        {
+            dateText.text = lastUsedDate.Date.ToString("MMMM") + " " + lastUsedDate.Date.ToString("dd") + GetDaySuffix(System.DateTime.DaysInMonth(lastUsedDate.Year, lastUsedDate.Month)) + ", " + lastUsedDate.Year.ToString();
+
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    string GetDaySuffix(int day)
+    {
+        switch (day)
+        {
+            case 1:
+            case 21:
+            case 31:
+                return "st";
+            case 2:
+            case 22:
+                return "nd";
+            case 3:
+            case 23:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
+}
